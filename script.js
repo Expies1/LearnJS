bdeb68f7f5e4e4115719aa425c31aaa2bf8491bc
Maxim <dobudogloM@gmail.com>
2021-03-11 18:40:18 +0300

    2 homevork

--- script.js
+++ script.js
@@ -12,14 +12,31 @@ let personalMovieDB = {
   privat: false
 };
 
-//3 
-const a = prompt('Один из последних просмотренных фильмов', '');
-const b = prompt('Оценка данного фильма', '');
-const c = prompt('Один из последних просмотренных фильмов', '');
-const d = prompt('Оценка данного фильма', '');
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Error");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//3 
+
+for (let i = 1; i < 3; i++) { 
+
+  const a = prompt('Один из последних просмотренных фильмов', '');
+  const b = prompt('Оценка данного фильма', '');
+  
+
+  if (a != '' && b != '' && a != null && b != null && a.length <= 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+     i--;
+     console.log("Error please write pole");
+  }
+}
 
 console.log(personalMovieDB.movies);
 
